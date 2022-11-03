6b18c7119d070acf99157e1bab8bd291ea41bf61
new2webdev22 <jf_22_fabris@hotmail.com>
2022-11-03 14:03:18 +0800

    Removing bootstraop form

---
+++ script.js
@@ -0,0 +1,9 @@
+let id = () =>{
+    
+}
+
+
+let username = document.getElementById("name"),
+ email = document.getElementById("email"),
+ subject = document.getElementById("subject"),
+ message = document.getElementById("message");
