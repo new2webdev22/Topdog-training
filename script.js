3c0066b52728975b40b09ce89d509642a767ded1
new2webdev22 <jf_22_fabris@hotmail.com>
2022-11-30 14:53:39 +0800

    form update

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-let id = () =>{
-    
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes)
+
+
+let username = id("username"),
+email = id("email"),
+number = id("number"),
+form = id("form"),
+errorMsg = classes("error"),
+successIcon = classes("success-icon"),
+faulureIcon = classes("failure-icon");
+
+form.addEventListener("submit", (e)=>{
+   e.preventDefault();
+
+   engine(username, 0, "Name cannot be blank");
+   engine(email, 1, "Email cannot be blank");
+   engine(number, 2, "Number cannot be blank");
+})
+
+let engine = (id, serial, message) =>{
+      if(id.value.trim() === ""){
+    errorMsg[serial].innerHTML = message;
+    faulureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+   }
+   else{
+    errorMsg[serial].innerHTML = "";
+    faulureIcon[serial].style.opacity = "0"
+    successIcon[serial].style.opacity = "1";
+   }
 }
-
-
-let username = document.getElementById("name"),
- email = document.getElementById("email"),
- subject = document.getElementById("subject"),
- message = document.getElementById("message");
